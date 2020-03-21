cadd96993030a49a016c15182065cb7e80e4ee1d
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2020-03-21 13:11:39 +0300

    Updated repository translation

--- DiscreteDistribution.cs
+++ DiscreteDistribution.cs
@@ -7,10 +7,10 @@ namespace Math
         private double _sum;
 
         /**
-         * The addItem method takes a String item as an input and if this map contains a mapping for the item it puts the item
-         * with given value + 1, else it puts item with value of 1.
+         * <summary>The addItem method takes a String item as an input and if this map contains a mapping for the item it puts the item
+         * with given value + 1, else it puts item with value of 1.</summary>
          *
-         * @param item String input.
+         * <param name="item">String input.</param>
          */
         public void AddItem(string item)
         {
@@ -27,10 +27,10 @@ namespace Math
         }
 
         /**
-         * The removeItem method takes a String item as an input and if this map contains a mapping for the item it puts the item
-         * with given value - 1, and if its value is 0, it removes the item.
+         * <summary>The removeItem method takes a String item as an input and if this map contains a mapping for the item it puts the item
+         * with given value - 1, and if its value is 0, it removes the item.</summary>
          *
-         * @param item String input.
+         * <param name="item">String input.</param>
          */
         public void RemoveItem(string item)
         {
@@ -45,11 +45,11 @@ namespace Math
         }
 
         /**
-         * The addDistribution method takes a {@link DiscreteDistribution} as an input and loops through the entries in this distribution
+         * <summary>The addDistribution method takes a {@link DiscreteDistribution} as an input and loops through the entries in this distribution
          * and if this map contains a mapping for the entry it puts the entry with its value + entry, else it puts entry with its value.
-         * It also accumulates the values of entries and assigns to the sum variable.
+         * It also accumulates the values of entries and assigns to the sum variable.</summary>
          *
-         * @param distribution {@link DiscreteDistribution} type input.
+         * <param name="distribution">{@link DiscreteDistribution} type input.</param>
          */
         public void AddDistribution(DiscreteDistribution distribution)
         {
@@ -69,11 +69,11 @@ namespace Math
         }
 
         /**
-         * The removeDistribution method takes a {@link DiscreteDistribution} as an input and loops through the entries in this distribution
+         * <summary>The removeDistribution method takes a {@link DiscreteDistribution} as an input and loops through the entries in this distribution
          * and if this map contains a mapping for the entry it puts the entry with its key - value, else it removes the entry.
-         * It also decrements the value of entry from sum and assigns to the sum variable.
+         * It also decrements the value of entry from sum and assigns to the sum variable.</summary>
          *
-         * @param distribution {@link DiscreteDistribution} type input.
+         * <param name="distribution">{@link DiscreteDistribution} type input.</param>
          */
         public void RemoveDistribution(DiscreteDistribution distribution)
         {
@@ -93,9 +93,9 @@ namespace Math
         }
 
         /**
-         * The getter for sum variable.
+         * <summary>The getter for sum variable.</summary>
          *
-         * @return sum.
+         * <returns>sum.</returns>
          */
         public double GetSum()
         {
@@ -103,10 +103,10 @@ namespace Math
         }
 
         /**
-         * The getIndex method takes an item as an input and returns the index of given item.
+         * <summary>The getIndex method takes an item as an input and returns the index of given item.</summary>
          *
-         * @param item to search for index.
-         * @return index of given item.
+         * <param name="item">to search for index.</param>
+         * <returns>index of given item.</returns>
          */
         public int GetIndex(string item)
         {
@@ -114,11 +114,11 @@ namespace Math
         }
 
         /**
-         * The containsItem method takes an item as an input and returns true if this map contains a mapping for the
-         * given item.
+         * <summary>The containsItem method takes an item as an input and returns true if this map contains a mapping for the
+         * given item.</summary>
          *
-         * @param item to check.
-         * @return true if this map contains a mapping for the given item.
+         * <param name="item">to check.</param>
+         * <returns>true if this map contains a mapping for the given item.</returns>
          */
         public bool ContainsItem(string item)
         {
@@ -126,10 +126,10 @@ namespace Math
         }
 
         /**
-         * The getItem method takes an index as an input and returns the item at given index.
+         * <summary>The getItem method takes an index as an input and returns the item at given index.</summary>
          *
-         * @param index is used for searching the item.
-         * @return the item at given index.
+         * <param name="index">is used for searching the item.</param>
+         * <returns>the item at given index.</returns>
          */
         public string GetItem(int index)
         {
@@ -137,10 +137,10 @@ namespace Math
         }
 
         /**
-         * The getValue method takes an index as an input and returns the value at given index.
+         * <summary>The getValue method takes an index as an input and returns the value at given index.</summary>
          *
-         * @param index is used for searching the value.
-         * @return the value at given index.
+         * <param name="index">is used for searching the value.</param>
+         * <returns>the value at given index.</returns>
          */
         public int GetValue(int index)
         {
@@ -148,11 +148,11 @@ namespace Math
         }
 
         /**
-         * The getCount method takes an item as an input returns the value to which the specified item is mapped, or {@code null}
-         * if this map contains no mapping for the key.
+         * <summary>The getCount method takes an item as an input returns the value to which the specified item is mapped, or {@code null}
+         * if this map contains no mapping for the key.</summary>
          *
-         * @param item is used to search for value.
-         * @return the value to which the specified item is mapped
+         * <param name="item">is used to search for value.</param>
+         * <returns>the value to which the specified item is mapped</returns>
          */
         public int GetCount(string item)
         {
@@ -160,9 +160,9 @@ namespace Math
         }
 
         /**
-         * The getMaxItem method loops through the entries and gets the entry with maximum value.
+         * <summary>The getMaxItem method loops through the entries and gets the entry with maximum value.</summary>
          *
-         * @return the entry with maximum value.
+         * <returns>the entry with maximum value.</returns>
          */
         public string GetMaxItem()
         {
@@ -181,15 +181,15 @@ namespace Math
         }
 
         /**
-         * Another getMaxItem method which takes an {@link ArrayList} of Strings. It loops through the items in this {@link ArrayList}
-         * and gets the item with maximum value.
+         * <summary>Another getMaxItem method which takes an {@link ArrayList} of Strings. It loops through the items in this {@link ArrayList}
+         * and gets the item with maximum value.</summary>
          *
-         * @param includeTheseOnly {@link ArrayList} of Strings.
-         * @return the item with maximum value.
+         * <param name="includeTheseOnly">{@link ArrayList} of Strings.</param>
+         * <returns>the item with maximum value.</returns>
          */
         public string GetMaxItem(List<string> includeTheseOnly)
         {
-            int max = -1;
+            var max = -1;
             string maxItem = null;
             foreach (var item in includeTheseOnly)
             {
@@ -210,11 +210,11 @@ namespace Math
         }
 
         /**
-         * The getProbability method takes an item as an input returns the value to which the specified item is mapped over sum,
-         * or 0.0 if this map contains no mapping for the key.
+         * <summary>The getProbability method takes an item as an input returns the value to which the specified item is mapped over sum,
+         * or 0.0 if this map contains no mapping for the key.</summary>
          *
-         * @param item is used to search for probability.
-         * @return the probability to which the specified item is mapped.
+         * <param name="item">is used to search for probability.</param>
+         * <returns>the probability to which the specified item is mapped.</returns>
          */
         public double GetProbability(string item)
         {
@@ -229,11 +229,11 @@ namespace Math
         }
 
         /**
-         * The getProbabilityLaplaceSmoothing method takes an item as an input returns the smoothed value to which the specified
-         * item is mapped over sum, or 1.0 over sum if this map contains no mapping for the key.
+         * <summary>The getProbabilityLaplaceSmoothing method takes an item as an input returns the smoothed value to which the specified
+         * item is mapped over sum, or 1.0 over sum if this map contains no mapping for the key.</summary>
          *
-         * @param item is used to search for probability.
-         * @return the smoothed probability to which the specified item is mapped.
+         * <param name="item">is used to search for probability.</param>
+         * <returns>the smoothed probability to which the specified item is mapped.</returns>
          */
         public double GetProbabilityLaplaceSmoothing(string item)
         {
@@ -246,9 +246,9 @@ namespace Math
         }
 
         /**
-         * The entropy method loops through the values and calculates the entropy of these values.
+         * <summary>The entropy method loops through the values and calculates the entropy of these values.</summary>
          *
-         * @return entropy value.
+         * <returns>entropy value.</returns>
          */
         public double Entropy()
         {
